ea75db427e3174a0636be0b69364b2cab2effa90
jcerva2338 <64172354+jcerva2338@users.noreply.github.com>
2023-09-18 19:31:36 -0500

    Added completed SQL file used for Tableau dashboard
    Added comments for documentation for SQL file used in the Tableau dashboard found here:
    https://public.tableau.com/app/profile/john.cervantes/viz/2020-21Top10U_SSalesFastFoodRestaurantsFranchisevs_CompanyStoresDashboard/2020-21TopFastFoodRestaurantsFranchisedvs_CompanyOwnedDashboard

--- JohnCervantes_top_50_fast_food_chains_dataset.sql
+++ JohnCervantes_top_50_fast_food_chains_dataset.sql
@@ -1,3 +1,7 @@
+-- Name: John Cervantes
+
+-- View the top 10 rows from the dataset
 SELECT TOP 10 * FROM ['Top 50 Fast-Food Chains in USA$'];
 
+-- See the top 10 rows from the dataset ordered by U.S. sales in descending order
 SELECT TOP 10 * FROM ['Top 50 Fast-Food Chains in USA$'] ORDER BY "U.S. Sales" DESC;
